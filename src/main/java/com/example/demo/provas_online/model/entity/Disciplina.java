472b4211153b970c4e27e6c6223f6deea6ed8ae7
thalesAlves758 <thalesalves758@gmail.com>
2022-12-10 11:46:29 -0300

    criar classe Disciplina

---
+++ src/main/java/com/example/demo/provas_online/model/entity/Disciplina.java
@@ -0,0 +1,24 @@
+package com.example.demo.provas_online.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Disciplina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String nome;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "disciplina")
+    private List<Prova> provas;
+}
