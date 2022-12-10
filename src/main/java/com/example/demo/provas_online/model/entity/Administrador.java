6d0dfc1dc44ddf29a8cd64b09832c98ec4e200e6
thalesAlves758 <thalesalves758@gmail.com>
2022-12-10 11:45:58 -0300

    criar classe Administrador

---
+++ src/main/java/com/example/demo/provas_online/model/entity/Administrador.java
@@ -0,0 +1,12 @@
+package com.example.demo.provas_online.model.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Administrador extends Usuario {
+}
