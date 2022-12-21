a03a8277b9ba5c01fecec67e5e55cc12d7b6386a
thalesAlves758 <thalesalves758@gmail.com>
2022-12-21 14:41:39 -0300

    criar DTO de disciplina

---
+++ src/main/java/com/example/demo/provas_online/api/dto/DisciplinaDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.provas_online.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DisciplinaDTO {
+    private Integer id;
+    private String nome;
+}
