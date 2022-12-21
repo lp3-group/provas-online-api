a7e3e715f51fa730bd19c3d23d806f3a030c1f13
thalesAlves758 <thalesalves758@gmail.com>
2022-12-21 11:16:25 -0300

    criar DTO de administrador

---
+++ src/main/java/com/example/demo/provas_online/api/dto/AdministradorDTO.java
@@ -0,0 +1,16 @@
+package com.example.demo.provas_online.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdministradorDTO {
+    private Integer id;
+    private String nome;
+    private String sobrenome;
+    private boolean primeiroAcesso;
+    private String nomeUsuario;
+}
