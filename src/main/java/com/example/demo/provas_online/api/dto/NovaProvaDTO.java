01c83932d24d31abca27823cd0f1a4c2d05da5c2
thalesAlves758 <thalesalves758@gmail.com>
2022-12-21 15:46:55 -0300

    criar dto de nova prova

---
+++ src/main/java/com/example/demo/provas_online/api/dto/NovaProvaDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.provas_online.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NovaProvaDTO {
+    private List<NovaQuestaoDTO> questoes;
+    private Integer disciplinaId;
+}
