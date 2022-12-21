b27d0a6026ffb9465aaab919f93cf7450bbfc01e
thalesAlves758 <thalesalves758@gmail.com>
2022-12-21 16:33:18 -0300

    adicionar verificacoes se ja existe prova com titulo especificado e nao existe disciplina

--- src/main/java/com/example/demo/provas_online/service/ProvaService.java
+++ src/main/java/com/example/demo/provas_online/service/ProvaService.java
@@ -1,20 +1,41 @@
 package com.example.demo.provas_online.service;
 
+import com.example.demo.provas_online.exception.DisciplinaNaoExisteException;
+import com.example.demo.provas_online.exception.ProvaJaExisteException;
+import com.example.demo.provas_online.model.entity.Disciplina;
 import com.example.demo.provas_online.model.entity.Prova;
+import com.example.demo.provas_online.model.repository.DisciplinaRepository;
 import com.example.demo.provas_online.model.repository.ProvaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class ProvaService {
     @Autowired
-    private ProvaRepository repository;
+    private ProvaRepository provaRepository;
 
-    public Prova criarProva(Prova prova) {
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
+    public Prova validarECriarProva(Prova prova) throws DisciplinaNaoExisteException, ProvaJaExisteException {
+        Optional<Disciplina> disciplina = disciplinaRepository.findById(prova.getDisciplina().getId());
+        Optional<Prova> provas = provaRepository.findByTitulo(prova.getTitulo());
+
+        if(disciplina.isEmpty()) {
+            throw new DisciplinaNaoExisteException();
+        }
+
+        if(provas.isPresent()) {
+            throw new ProvaJaExisteException();
+        }
+
+        prova.setId(null);
         prova.setCriadaEm(new Date());
+        prova.setDisciplina(disciplina.get());
 
-        return repository.save(prova);
+        return provaRepository.save(prova);
     }
 }
