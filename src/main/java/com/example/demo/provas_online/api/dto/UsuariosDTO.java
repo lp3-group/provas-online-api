3da8a7abab4c77e42ab582f2ae5f98e91d38f083
thalesAlves758 <thalesalves758@gmail.com>
2022-12-21 11:16:42 -0300

    criar DTO dos usuarios

---
+++ src/main/java/com/example/demo/provas_online/api/dto/UsuariosDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.provas_online.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuariosDTO {
+    private List<AdministradorDTO> administradores;
+    private List<EstudanteDTO> estudantes;
+}
