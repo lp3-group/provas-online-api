1e78a212879245e3977ac43b1156347b32e2cb99
thalesAlves758 <thalesalves758@gmail.com>
2022-12-23 11:32:12 -0300

    criar uma rota para pegar uma prova pelo id

--- src/main/java/com/example/demo/provas_online/api/controller/ProvaController.java
+++ src/main/java/com/example/demo/provas_online/api/controller/ProvaController.java
@@ -5,6 +5,7 @@ import com.example.demo.provas_online.api.dto.NovaProvaDTO;
 import com.example.demo.provas_online.api.dto.ProvaDTO;
 import com.example.demo.provas_online.exception.DisciplinaNaoExisteException;
 import com.example.demo.provas_online.exception.ProvaJaExisteException;
+import com.example.demo.provas_online.exception.ProvaNaoExisteException;
 import com.example.demo.provas_online.model.entity.Prova;
 import com.example.demo.provas_online.service.ProvaService;
 import com.example.demo.provas_online.utility.MapeadorDeListas;
@@ -51,5 +52,18 @@ public class ProvaController {
         List<ListaProvasDTO> retorno = MapeadorDeListas.mapeiaListaParaListaDeDTO(provas, ListaProvasDTO.class);
 
         return ResponseEntity.ok(retorno);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity obterProvaPeloId(@PathVariable("id") Integer id) {
+        try {
+            Prova prova = service.validarEObterProvaPeloId(id);
+
+            ProvaDTO retorno = modelMapper.map(prova, ProvaDTO.class);
+
+            return ResponseEntity.ok(retorno);
+        } catch (ProvaNaoExisteException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
