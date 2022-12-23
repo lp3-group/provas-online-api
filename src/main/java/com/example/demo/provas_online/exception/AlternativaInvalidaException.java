e0379280b3a8de971a7350e7bbfc8eb8deb72475
thalesAlves758 <thalesalves758@gmail.com>
2022-12-23 17:48:50 -0300

    criar excecao de alternativa invalida

---
+++ src/main/java/com/example/demo/provas_online/exception/AlternativaInvalidaException.java
@@ -0,0 +1,7 @@
+package com.example.demo.provas_online.exception;
+
+public class AlternativaInvalidaException extends RuntimeException {
+    public AlternativaInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+}
