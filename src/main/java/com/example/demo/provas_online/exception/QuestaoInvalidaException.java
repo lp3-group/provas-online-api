8ded1e7bfc3706fd5e480984baa338e3ed45afe6
thalesAlves758 <thalesalves758@gmail.com>
2022-12-23 17:49:01 -0300

    criar excecao de questao invalida

---
+++ src/main/java/com/example/demo/provas_online/exception/QuestaoInvalidaException.java
@@ -0,0 +1,7 @@
+package com.example.demo.provas_online.exception;
+
+public class QuestaoInvalidaException extends RuntimeException {
+    public QuestaoInvalidaException(String messagem) {
+        super(messagem);
+    }
+}
