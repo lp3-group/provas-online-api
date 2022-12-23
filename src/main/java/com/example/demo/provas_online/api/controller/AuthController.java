5d54f0a2ee9376ac92818f57756a345c01c1fb37
thalesAlves758 <thalesalves758@gmail.com>
2022-12-23 17:22:44 -0300

    criar rota para alterar senha

--- src/main/java/com/example/demo/provas_online/api/controller/AuthController.java
+++ src/main/java/com/example/demo/provas_online/api/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.demo.provas_online.api.controller;
 
+import com.example.demo.provas_online.api.dto.AlterarSenhaDTO;
 import com.example.demo.provas_online.api.dto.LoginDTO;
 import com.example.demo.provas_online.api.dto.LoginRetornoDTO;
 import com.example.demo.provas_online.exception.SenhaInvalidaException;
@@ -13,6 +14,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,6 +45,19 @@ public class AuthController {
             return ResponseEntity.ok(retorno);
         } catch (UsernameNotFoundException | SenhaInvalidaException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PutMapping("/alterarSenha")
+    public ResponseEntity alterarSenha(@RequestBody AlterarSenhaDTO corpoRequisicao, Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        try {
+            authService.validarSenha(corpoRequisicao.getSenha(), corpoRequisicao.getConfirmaSenha());
+            authService.alterarSenha(userDetails.getUsername(), corpoRequisicao.getSenha());
+
+            return ResponseEntity.noContent().build();
+        } catch (SenhaInvalidaException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 }
