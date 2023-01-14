ff1cc69cb12c8d161fc24fb19df6026b6c6b8567
thalesAlves758 <thalesalves758@gmail.com>
2023-01-14 11:17:54 -0300

    adicionar notação @Builder

--- src/main/java/com/example/demo/provas_online/model/entity/ProvaRealizada.java
+++ src/main/java/com/example/demo/provas_online/model/entity/ProvaRealizada.java
@@ -1,6 +1,7 @@
 package com.example.demo.provas_online.model.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 public class ProvaRealizada {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
