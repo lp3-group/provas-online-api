b014f8aea512ad02faf3628784ce39135a228bde
thalesAlves758 <thalesalves758@gmail.com>
2023-01-14 11:19:03 -0300

    criar repository de prova realizada

---
+++ src/main/java/com/example/demo/provas_online/model/repository/ProvaRealizadaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.provas_online.model.repository;
+
+import com.example.demo.provas_online.model.entity.ProvaRealizada;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvaRealizadaRepository extends JpaRepository<ProvaRealizada, Integer> {
+}
