d78b5a567abfc4ea1a4140a22eeefc1814027eec
thalesAlves758 <thalesalves758@gmail.com>
2023-01-14 11:18:54 -0300

    criar repository de alternativa

---
+++ src/main/java/com/example/demo/provas_online/model/repository/AlternativaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.provas_online.model.repository;
+
+import com.example.demo.provas_online.model.entity.Alternativa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlternativaRepository extends JpaRepository<Alternativa, Integer> {
+}
