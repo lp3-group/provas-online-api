00328e71358985fb1899a05aea475d87586d0474
thalesAlves758 <thalesalves758@gmail.com>
2023-01-18 20:13:44 -0300

    consertar mensagem

--- src/main/java/com/example/demo/provas_online/exception/UsuarioNaoExisteException.java
+++ src/main/java/com/example/demo/provas_online/exception/UsuarioNaoExisteException.java
@@ -2,6 +2,6 @@ package com.example.demo.provas_online.exception;
 
 public class UsuarioNaoExisteException extends RuntimeException {
     public UsuarioNaoExisteException() {
-        super("Este não usuário existe");
+        super("Este usuário não existe");
     }
 }
