97f5f74ca3bb1dfbe3569689cf6c169d2583d257
thalesAlves758 <thalesalves758@gmail.com>
2023-01-20 23:40:33 -0300

    testar prova encontrada ao tentar criar

--- src/test/java/com/example/demo/provas_online/service/ProvaServiceTest.java
+++ src/test/java/com/example/demo/provas_online/service/ProvaServiceTest.java
@@ -4,6 +4,7 @@ import static org.easymock.EasyMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.demo.provas_online.exception.DisciplinaNaoExisteException;
+import com.example.demo.provas_online.exception.ProvaJaExisteException;
 import com.example.demo.provas_online.model.entity.Disciplina;
 import com.example.demo.provas_online.model.entity.Prova;
 import com.example.demo.provas_online.model.repository.DisciplinaRepository;
@@ -32,7 +33,7 @@ public class ProvaServiceTest {
         provaTeste.setDisciplina(disciplinaTeste);
 
         DisciplinaRepository disciplinaRepositoryMock = createMock(DisciplinaRepository.class);
-        expect(disciplinaRepositoryMock.findById(provaTeste.getId())).andReturn(Optional.ofNullable(null));
+        expect(disciplinaRepositoryMock.findById(provaTeste.getDisciplina().getId())).andReturn(Optional.ofNullable(null));
         replay(disciplinaRepositoryMock);
 
         ProvaRepository provaRepositoryMock = createMock(ProvaRepository.class);
@@ -42,6 +43,30 @@ public class ProvaServiceTest {
             fail();
         } catch (DisciplinaNaoExisteException e) {
             assertEquals("Disciplina não encontrada!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testaProvaEncontradaAoTentarCriar() {
+        Disciplina disciplinaTeste = new Disciplina();
+        disciplinaTeste.setId(anyInt());
+
+        Prova provaTeste = new Prova();
+        provaTeste.setDisciplina(disciplinaTeste);
+
+        DisciplinaRepository disciplinaRepositoryMock = createMock(DisciplinaRepository.class);
+        expect(disciplinaRepositoryMock.findById(provaTeste.getDisciplina().getId())).andReturn(Optional.ofNullable(disciplinaTeste));
+        replay(disciplinaRepositoryMock);
+
+        ProvaRepository provaRepositoryMock = createMock(ProvaRepository.class);
+        expect(provaRepositoryMock.findByTitulo(provaTeste.getTitulo())).andReturn(Optional.ofNullable(provaTeste));
+        replay(provaRepositoryMock);
+
+        try {
+            this.provaService.validarECriarProva(provaTeste, disciplinaRepositoryMock, provaRepositoryMock);
+            fail();
+        } catch (ProvaJaExisteException e) {
+            assertEquals("Esta prova já existe!", e.getMessage());
         }
     }
 }
