9bdbaef98fc2821fafd16efcf00d589bf2356dea
thalesAlves758 <thalesalves758@gmail.com>
2023-01-23 08:11:06 -0300

    testar erro por nao encontrar disciplina ao atualizar uma prova

--- src/test/java/com/example/demo/provas_online/service/ProvaServiceTest.java
+++ src/test/java/com/example/demo/provas_online/service/ProvaServiceTest.java
@@ -286,8 +286,10 @@ public class ProvaServiceTest {
         expect(provaRepositoryMock.findById(id)).andReturn(Optional.ofNullable(null));
         replay(provaRepositoryMock);
 
+        DisciplinaRepository disciplinaRepository = createMock(DisciplinaRepository.class);
+
         try {
-            this.provaService.validarEAtualizarProva(prova, provaRepositoryMock);
+            this.provaService.validarEAtualizarProva(prova, provaRepositoryMock, disciplinaRepository);
             fail();
         } catch (ProvaNaoExisteException e) {
             assertEquals("Prova não encontrada!", e.getMessage());
@@ -307,11 +309,53 @@ public class ProvaServiceTest {
         expect(provaRepositoryMock.findByTitulo(prova.getTitulo())).andReturn(Optional.ofNullable(provaEncontradaComMesmoTitulo));
         replay(provaRepositoryMock);
 
+        DisciplinaRepository disciplinaRepository = createMock(DisciplinaRepository.class);
+
         try {
-            this.provaService.validarEAtualizarProva(prova, provaRepositoryMock);
+            this.provaService.validarEAtualizarProva(prova, provaRepositoryMock, disciplinaRepository);
             fail();
         } catch (ProvaJaExisteException e) {
             assertEquals("Esta prova já existe!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testaErroPorNaoEncontrarADisciplina() {
+        Disciplina disciplinaTeste = new Disciplina();
+        disciplinaTeste.setId(anyInt());
+
+        Alternativa alternativaTeste1 = new Alternativa();
+        alternativaTeste1.setRespostaCerta(true);
+        Alternativa alternativaTeste2 = new Alternativa();
+        alternativaTeste2.setRespostaCerta(false);
+
+        List<Alternativa> alternativasTeste = new ArrayList<Alternativa>();
+        alternativasTeste.add(alternativaTeste1);
+        alternativasTeste.add(alternativaTeste2);
+
+        Questao questaoTeste = new Questao();
+        questaoTeste.setAlternativas(alternativasTeste);
+        List<Questao> questoesTeste = new ArrayList<Questao>();
+        questoesTeste.add(questaoTeste);
+
+        Prova provaTeste = new Prova();
+        provaTeste.setDisciplina(disciplinaTeste);
+        provaTeste.setQuestoes(questoesTeste);
+
+        ProvaRepository provaRepositoryMock = createMock(ProvaRepository.class);
+        expect(provaRepositoryMock.findById(provaTeste.getId())).andReturn(Optional.ofNullable(provaTeste));
+        expect(provaRepositoryMock.findByTitulo(provaTeste.getTitulo())).andReturn(Optional.ofNullable(null));
+        replay(provaRepositoryMock);
+
+        DisciplinaRepository disciplinaRepositoryMock = createMock(DisciplinaRepository.class);
+        expect(disciplinaRepositoryMock.findById(disciplinaTeste.getId())).andReturn(Optional.ofNullable(null));
+        replay(disciplinaRepositoryMock);
+
+        try {
+            this.provaService.validarEAtualizarProva(provaTeste, provaRepositoryMock, disciplinaRepositoryMock);
+            fail();
+        } catch (DisciplinaNaoExisteException e) {
+            assertEquals("Disciplina não encontrada!", e.getMessage());
         }
     }
 }
